1d139162be1f1f3667688160727302f2b0afcbb0
Alex Halverson <alexhalverson@posteo.net>
2023-08-03 21:46:42 +1000

    feat: add category shema

---
+++ schemas/category.ts
@@ -0,0 +1,44 @@
+import { TagIcon } from '@sanity/icons'
+import { defineField, defineType } from 'sanity'
+
+export default defineType({
+  name: 'category',
+  title: 'Category',
+  icon: TagIcon,
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 96,
+        isUnique: (value, context) => context.defaultIsUnique(value, context),
+      },
+      validation: (rule) => rule.required(),
+    }),
+
+    defineField({
+      name: 'coverImage',
+      title: 'Cover Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+      validation: (rule) => rule.required(),
+    }),
+  ],
+})
